46665e2dd83cf84621bd01f04789b2c67377c7f5
ZharkovMihail <56646565m@mail.ru>
2021-02-26 00:11:15 +0300

    refactoring hime_page with template

--- lists/views.py
+++ lists/views.py
@@ -5,4 +5,4 @@ from django.shortcuts import render
 def home_page(request):
     """домашняя страница"""
 
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
