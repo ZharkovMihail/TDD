c78719303124a49840df2912951e3ce8f1def81e
ZharkovMihail <56646565m@mail.ru>
2021-02-25 21:26:14 +0300

    FT now check adding task to list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -19,14 +21,27 @@ class NewVisitorTest(unittest.TestCase):
         # ну и решил почекать его домашнюю страницу
         self.browser.get('http://localhost:8000')
 
-        # Он убеждается что заголовок про неотложные дела
+        # Он убеждается что заголовок и шапка страницы про неотложные дела
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # ему предлогоается ввести элемент списка
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
         # набирает в текстовом поле "купить угольки для кэлыча"
+        input_box.send_keys('купить угольки для кэлыча')
 
         # после ENTER страница обновляется и появляется список с одним пунктом: "1: купить угольки для кэлыча"
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1: купить угольки для кэлыча' for row in rows))
+
+        self.fail('Закончить тест')
         # текстовое поле предлагает ввести ещё дело
         # Вася вводит "Раскумарить плотную забивочку"
 
