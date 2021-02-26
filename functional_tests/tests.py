27924d5d80c3062f8ee6923aa5062570cc10f777
ZharkovMihail <56646565m@mail.ru>
2021-02-27 02:45:34 +0300

    upgrade functianal test by testing multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -66,10 +66,52 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: купить угольки для кэлыча')
         self.wait_for_row_in_list_table('2: Раскумарить плотную забивочку')
 
-        self.fail('Закончить тест')
         # Васе интересно запомнит ли сайт его ценные планы. Он видит, что сайт сгенерировал для него уникальный
         # URL-адрес с пояснением зачем это
 
         # Вася чекает ссылку, и рад что это не сайт со слатами где макака ловит банан, а его список дел
 
         # довольный, идет запускать катку в доте
+
+    def test_multiple_users_can_startlists_at_different_urls(self):
+        """тест: многочиленные пользователи могут начать списки по разным url"""
+        # Вася начинает новый список
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('купить угольки для кэлыча')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: купить угольки для кэлыча')
+
+        # Он видит что его список имеет уникальный url
+        vasy_list_url = self.browser.current_url
+        self.assertRegex(vasy_list_url, '/lists/.+')
+
+        # Теперь новый пользователь Семен заходт на сайт
+
+        # # Используем новый сеанс браузера, чтобы никакая информация Васи не прошла через куки
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Семён посещает домашнюю страницу. Нет никаких признаков списка Васи
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('купить угольки для кэлыча', page_text)
+
+        # Семен начинает новый список, вводя новый элемент.
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('сдать на права')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: сдать на права')
+
+        # Семён получает уникальный URL
+        semen_list_url = self.browser.current_url
+        self.assertRegex(semen_list_url, '/lists/.+')
+        self.assertNotEqual(semen_list_url, vasy_list_url)
+
+        # опять нет ни слида списка васи
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('купить угольки для кэлыча', page_text)
+        self.assertIn('сдать на права', page_text)
+
+        # довольные, идут в пати в дотку
