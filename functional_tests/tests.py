488851662a9279761d8187dae4fc06d24c24ae8d
ZharkovMihail <56646565m@mail.ru>
2021-02-26 18:47:29 +0300

    def wait_for_... better than sleep

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
 from django.test import LiveServerTestCase
+
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -15,11 +19,19 @@ class NewVisitorTest(LiveServerTestCase):
         """демонтаж"""
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        """подтверждение строки в таблице списка"""
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        """ожидать строку в таблице списка"""
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
@@ -41,20 +53,18 @@ class NewVisitorTest(LiveServerTestCase):
 
         # после ENTER страница обновляется и появляется список с одним пунктом: "1: купить угольки для кэлыча"
         input_box.send_keys(Keys.ENTER)
-        time.sleep(5)
 
-        self.check_for_row_in_list_table('1: купить угольки для кэлыча')
+        self.wait_for_row_in_list_table('1: купить угольки для кэлыча')
 
         # текстовое поле предлагает ввести ещё дело
         # Вася вводит "Раскумарить плотную забивочку"
         input_box = self.browser.find_element_by_id('id_new_item')
         input_box.send_keys('Раскумарить плотную забивочку')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # Страница обновляется и показывает список с двумя задачами
-        self.check_for_row_in_list_table('1: купить угольки для кэлыча')
-        self.check_for_row_in_list_table('2: Раскумарить плотную забивочку')
+        self.wait_for_row_in_list_table('1: купить угольки для кэлыча')
+        self.wait_for_row_in_list_table('2: Раскумарить плотную забивочку')
 
         self.fail('Закончить тест')
         # Васе интересно запомнит ли сайт его ценные планы. Он видит, что сайт сгенерировал для него уникальный
@@ -63,7 +73,3 @@ class NewVisitorTest(LiveServerTestCase):
         # Вася чекает ссылку, и рад что это не сайт со слатами где макака ловит банан, а его список дел
 
         # довольный, идет запускать катку в доте
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
