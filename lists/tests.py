ccd0a16a370294723ec5c52189be700bd0e4f0b4
ZharkovMihail <56646565m@mail.ru>
2021-02-27 06:06:43 +0300

    get parametr from url adress

--- lists/tests.py
+++ lists/tests.py
@@ -49,19 +49,25 @@ class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         """тест: используется шаблон списка"""
-        response = self.client.get('/lists/lol_kek/')
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_items(self):
-        """тест: отображаются все элементы списка"""
-        list_ = List.objects.create()
-        Item.objects.create(text='item 1', list=list_)
-        Item.objects.create(text='item 2', list=list_)
+    def test_displays_only_items_for_that_list(self):
+        """тест: отображаются элементы только для этого списка"""
+        correct_list = List.objects.create()
+        Item.objects.create(text='item 1', list=correct_list)
+        Item.objects.create(text='item 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='another item 1', list=other_list)
+        Item.objects.create(text='another item 2', list=other_list)
 
-        response = self.client.get('/lists/lol_kek/')
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'item 1')
         self.assertContains(response, 'item 2')
+        self.assertNotContains(response, 'another item 1')
+        self.assertNotContains(response, 'another item 2')
 
     def test_can_save_a_POST_request(self):
         """тест: можно сохранять post-запрос"""
@@ -73,4 +79,5 @@ class ListViewTest(TestCase):
     def test_redirects_after_POST(self):
         """тест: переадресует после POST-запроса"""
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response, '/lists/lol_kek/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
